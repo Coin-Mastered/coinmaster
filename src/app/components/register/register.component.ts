75f38987e7f0b2e07e9b69be9d9b14d4f08932e1
silverrock801 <jamesp801@gmail.com>
2022-01-18 18:04:27 -0500

    added register user logic

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,33 @@
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User, Wallet } from 'src/app/models/user';
+import { ClientMessage } from 'src/app/models/client-message';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent{
 
-  constructor() { }
+  title = 'Register'
+  public user = new User(0, '', '', '', '', '')
+  public wallet = new Wallet;
+  public clientMessage = new ClientMessage('')
 
-  ngOnInit(): void {
+
+  constructor(private userService: UserService) { }
+
+  public registerUser(): void{
+
+    this.user.wallet.push(this.wallet);
+
+    this.userService.registerUser(this.user)
+      .subscribe(
+        data => this.clientMessage.message = `Successfully registersed ${data.username}`,
+        error => this.clientMessage.message = `Something went wrong. Error: ${error}`
+      )
   }
+
 
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,31 @@
+export class User{
+
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+  wallet: any;
+
+
+  constructor(
+    id: number,
+    firstName: string,
+    lastName: string,
+    username: string,
+    password: string,
+    email: string,
+) {
+    this.id = id
+    this.firstName = firstName
+    this.lastName = lastName
+    this.username = username
+    this.password = password
+    this.email = email
+}
+  }
+
+  export class Wallet{
+
+  }

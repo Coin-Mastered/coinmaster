1d8e3a247b5595fb06cae4b34f158b104f8c9aaf
vrlx3 <bhavsar.viral@outlook.com>
2022-01-21 16:06:55 -0600

    Added prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
 };
+export const awsUrl = 'http://ec2-52-90-7-20.compute-1.amazonaws.com:5000/';
+export const coinurl = 'https://api.coinbase.com/v2/prices/';

5891b4a91d40df022aa9fec2aefeafa38f52a5b2
vrlx3 <bhavsar.viral@outlook.com>
2022-01-24 14:43:36 -0600

    Docker File

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 };
-export const awsUrl = 'http://ec2-52-90-7-20.compute-1.amazonaws.com:5000/';
+export const awsUrl = 'http://localhost:5000/';
 export const coinurl = 'https://api.coinbase.com/v2/prices/';
